ddff224b87347b43b656f4f41c5f762d42c1d33b
Denis Volnenko <volnenko@smartsolutions-123.ru>
2015-04-16 17:12:18 +0400

    Added Digest Util

---
+++ source/src/main/java/com/orioncompany/lib/util/data/DigestUtil.java
@@ -0,0 +1,46 @@
+package com.orioncompany.lib.util.data;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by orion on 16.04.15.
+ */
+public class DigestUtil {
+
+    public static String getHash(
+            @Nullable final String algorithm,
+            @Nullable final String value
+    ) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        if (value == null || algorithm == null) {
+            return null;
+        }
+        final MessageDigest md = MessageDigest.getInstance(algorithm);
+        final byte[] digest = md.digest(value.getBytes("UTF-8"));
+        return digestToString(digest);
+    }
+
+    @Nullable
+    public static String sha256(@Nullable final String value) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        return getHash("SHA-256", value);
+    }
+
+    @Nullable
+    public static String md5(@Nullable final String value) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        return getHash("MD5", value);
+    }
+
+    @NotNull
+    private static String digestToString(@NotNull final byte[] digest) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (final byte anArray : digest) {
+            stringBuilder.append(Integer.toHexString((anArray & 0xFF) | 0x100).substring(1, 3));
+        }
+        return stringBuilder.toString();
+    }
+
+}
